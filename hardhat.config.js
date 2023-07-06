e482407f58f61ffadc83b90ffafe5fc4d44742c8
sobbey99 <sobbey99@gmail.com>
2023-07-06 18:44:02 +0300

    add deploying token & nft script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    numbai: {
+      url: process.env.MUMBAI_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
